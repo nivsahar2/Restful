067868a0e96a44b867a647d9cc73bb9bb88d05ad
nivsahar2 <nivsahar2@gmail.com>
2022-09-29 16:38:40 +0300

    Added new Services

--- src/main/java/com/example/restful/controller/HelloController.java
+++ src/main/java/com/example/restful/controller/HelloController.java
@@ -1,2 +1,68 @@
-package com.example.restful.controller;public class HelloController {
+package com.example.restful.controller;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import netscape.javascript.JSObject;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@RestController
+public class HelloController {
+
+    String file = "src/main/resources/CV.jpg";
+    @RequestMapping("/Weather")
+    public String getWeather(@RequestParam String Location) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://weatherapi-com.p.rapidapi.com/current.json?q=q%3D" + Location))
+                .header("X-RapidAPI-Key", "862eec23ddmsh5dce13392fe5f60p16c72djsn3cfcdd8fa329")
+                .header("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        return  response.body();
+    }
+    @RequestMapping(value = "/CV", method = RequestMethod.GET)
+    public void getImageAsByteArray(HttpServletResponse response) throws IOException {
+        File f = new File(file);
+        if(f.exists() && !f.isDirectory()) {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            BufferedImage img = ImageIO.read(new File(file));
+            ImageIO.write(img,file.substring(file.lastIndexOf('.') + 1), os);
+            InputStream in = new ByteArrayInputStream(os.toByteArray());
+            response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+            IOUtils.copy(in, response.getOutputStream());
+        }
+    }
+    @RequestMapping("/TimeZone")
+    public String getTimeZone(@RequestParam String Location) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://weatherapi-com.p.rapidapi.com/timezone.json?q=q%3D" + Location))
+                .header("X-RapidAPI-Key", "862eec23ddmsh5dce13392fe5f60p16c72djsn3cfcdd8fa329")
+                .header("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        return  response.body();
+    }
 }
